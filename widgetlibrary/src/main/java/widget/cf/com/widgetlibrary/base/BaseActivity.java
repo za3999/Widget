cf38ec5e8c9bb0f5f340b4793e9232ad4d4a2b64
zhengcaifu <344984602@qq.com>
2020-01-02 15:05:00 +0800

    StatusBarUtil

--- widgetlibrary/src/main/java/widget/cf/com/widgetlibrary/base/BaseActivity.java
+++ widgetlibrary/src/main/java/widget/cf/com/widgetlibrary/base/BaseActivity.java
@@ -1,11 +1,20 @@
 package widget.cf.com.widgetlibrary.base;
 
 import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
 import widget.cf.com.widgetlibrary.language.MultiLanguageUtil;
+import widget.cf.com.widgetlibrary.util.StatusBarUtil;
 
 public class BaseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        StatusBarUtil.setTranslucentStatus(this, isTopOffset());
+    }
 
     @Override
     protected void attachBaseContext(Context newBase) {
@@ -13,4 +22,7 @@ public class BaseActivity extends AppCompatActivity {
         MultiLanguageUtil.attachBaseContext(this);
     }
 
+    protected boolean isTopOffset() {
+        return true;
+    }
 }
---
+++ widgetlibrary/src/main/java/widget/cf/com/widgetlibrary/util/StatusBarUtil.java
@@ -0,0 +1,66 @@
+package widget.cf.com.widgetlibrary.util;
+
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Color;
+import android.os.Build;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class StatusBarUtil {
+
+    public static void setTranslucentStatus(Activity activity, boolean needOffset) {
+        setTranslucentStatus(activity.getWindow(), needOffset);
+    }
+
+    public static void setTranslucentStatus(Window window, boolean needOffset) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            View decorView = window.getDecorView();
+            int option = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                option = option | View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR;
+            }
+            decorView.setSystemUiVisibility(option);
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(Color.TRANSPARENT);
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            WindowManager.LayoutParams attributes = window.getAttributes();
+            int flagTranslucentStatus = WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS;
+            attributes.flags |= flagTranslucentStatus;
+            window.setAttributes(attributes);
+        }
+        if (needOffset) {
+            ViewGroup contentView = window.getDecorView().findViewById(android.R.id.content);
+            ViewGroup.MarginLayoutParams layoutParams = (ViewGroup.MarginLayoutParams) contentView.getLayoutParams();
+            layoutParams.setMargins(layoutParams.leftMargin, layoutParams.topMargin + getStatusBarHeight(window.getContext()),
+                    layoutParams.rightMargin, layoutParams.bottomMargin);
+        }
+    }
+
+    public static void setOffsetView(View view){
+        view.setPadding(view.getPaddingLeft(), view.getPaddingTop() + getStatusBarHeight(view.getContext()), view.getPaddingRight(), view.getPaddingBottom());
+    }
+
+    public static int getStatusBarHeight(Context context) {
+        int result = 0;
+        int resourceId = context.getResources().getIdentifier("status_bar_height",
+                "dimen", "android");
+        if (resourceId > 0) {
+            result = context.getResources().getDimensionPixelSize(resourceId);
+        }
+        return result;
+    }
+
+    public static int getNavigationBarHeight(Context context) {
+        Resources resources = context.getResources();
+        int resourceId = resources.getIdentifier("navigation_bar_height", "dimen", "android");
+        int height = resources.getDimensionPixelSize(resourceId);
+        return height;
+    }
+
+}
+
